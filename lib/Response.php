5320d8a04c8a59156a946687ce875ebf5cd43087
Matheus Amorim <amorimm101@gmail.com>
2025-04-08 01:47:44 -0300

    atualizando dominio do app na vercel

--- lib/Response.php
+++ lib/Response.php
@@ -3,7 +3,7 @@ class Response {
     public static function json($data, $status = 200) {
         http_response_code($status);
         header('Content-Type: application/json; charset=utf-8');
-        header('Access-Control-Allow-Origin: https://frontend-challenge-three-delta.vercel.app/');
+        header('Access-Control-Allow-Origin: https://frontend-challenge-three-delta.vercel.app');
         echo json_encode($data);
         exit;
     }
